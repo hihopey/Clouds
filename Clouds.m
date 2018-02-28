998f26764130cf177279a15eb58e0db83d1fc7f0
hihopey <hopey.schneider@gmail.com>
2018-02-28 14:17:50 -0600

    Add files via upload

--- Clouds.m
+++ Clouds.m
@@ -1,6 +1,6 @@
 clear all
 % radar frequency (Hz);
-freq = linspace(0.1, 100e9, 100);
+freq = linspace(0.1, 200e9, 1000);
 c = 3e11; %speed of light in mm
 % radar wavelength (mm);
 lambda = c./freq;
@@ -60,10 +60,12 @@ sig_s = zeros(nl, nd);
 sig_an = zeros(nl, nd);
 sig_sn = zeros(nl, nd);
 sig_ed = zeros(nl, nd);
-att = zeros(nl);
+att = zeros(nl, 10);
 Nd = zeros(nl, nd);
-trans = zeros(nl, 6);
-
+trans = zeros(nl, 10);
+LWC = [0.28 0.4 0.3 0.26 0.41 0.28 0.41 0.45 0.3 0.33];
+a0 = [5.15 7.73 4.09 2.17 6.04 7 7.74 7.38 3.11 3.51];
+u = [10 16.3 2.5 1.6 15.7 7 7.6 4.8 2.8 3.2];
 %Ask user which temperature index they want to use
 fprintf(1, 'Possible temperature values\n')
 fprintf(1, '1: -10C\n')
@@ -84,30 +86,43 @@ switch ti
   fprintf('*** Invalid Selection ... exiting!\n');
   return
 end
-   
+  for i = 1 : 10 
     for lambi = 1 : nl
         for D_n_ind = 1: nd
             D=lambda(lambi)*D_n(D_n_ind)/pi;
-            a = D/2;
+            a = (D_n(D_n_ind))/2;
             k = (2 * pi)/(lambda(lambi));
             x = k * a;
             m = m_w(lambi, ti);
             result = Mie(m, x);
             E = result(:, 4) .* (pi*(a^2));
             sig_e = sum(E);
-            Nd = (10^10) * (exp(-100*D)); 
-            att(lambi) = att(lambi) + (4.34e3 * Nd * sig_e * deltaD); % calculates attenuation
-            trans(lambi) = 1 - att(lambi); % calculates transmission
+            N = (u(i)^(u(i) + 1))/(gamma(u(i) + 1) * (a0(i)^(u(i) + 1))); 
+            Nd = (N * (a^u(i))) * exp(-u(i)*(a/a0(i))); 
+            att(lambi, i) = att(lambi, i) + (4.34e3 * Nd * sig_e * deltaD); % calculates attenuation
+            trans(lambi, i) = 1 - att(lambi, i); % calculates transmission
         end
     end
+  end
     
 % Print Attenuation Graph
 fntsz = 14;
 figure(1)
 clf
-ylim ([2 10])
-plot (freq/1e9, att(:, 1), 'b')
+plot (freq/1e9, att(:, 1), 'b', 'DisplayName', '0.28 g/cm^3')
 hold on
+plot (freq/1e9, att(:, 2), 'r', 'DisplayName', '0.4 g/cm^3')
+plot (freq/1e9, att(:, 3), 'k', 'DisplayName', '0.3 g/cm^3')
+plot (freq/1e9, att(:, 4), 'g', 'DisplayName', '0.26 g/cm^3')
+plot (freq/1e9, att(:, 5), 'r--', 'DisplayName', '0.41 g/cm^3')
+plot (freq/1e9, att(:, 6), 'b', 'DisplayName', '0.28 g/cm^3')
+plot (freq/1e9, att(:, 7), 'r--', 'DisplayName', '0.41 g/cm^3')
+plot (freq/1e9, att(:, 8), 'k--', 'DisplayName', '0.45 g/cm^3')
+plot (freq/1e9, att(:, 9), 'k', 'DisplayName', '0.3 g/cm^3')
+plot (freq/1e9, att(:, 10), 'g--', 'DisplayName', '0.33 g/cm^3')
+%ylim ([2 10])
+lgd = legend('location', 'northwest', 'orientation', 'vertical')
+title(lgd, 'LWC')
 set(gca, 'fontsize', fntsz)
 xlabel('Frequency (GHz)')
 ylabel('Attenuation (dB / Km)')
@@ -115,12 +130,22 @@ title(ptit)
 hold off
   
 % Print Transmission Graph
-fntsz = 14;
 figure(2)
 clf
-ylim ([-10 -2])
-plot (freq./1e9, trans(:, 1), 'b')
+plot (freq./1e9, trans(:, 1), 'b', 'DisplayName', '5mm')
 hold on
+plot (freq/1e9, trans(:, 2), 'r', 'DisplayName', '0.4 g/cm^3')
+plot (freq/1e9, trans(:, 3), 'k', 'DisplayName', '0.3 g/cm^3')
+plot (freq/1e9, trans(:, 4), 'g', 'DisplayName', '0.26 g/cm^3')
+plot (freq/1e9, trans(:, 5), 'r--', 'DisplayName', '0.41 g/cm^3')
+plot (freq/1e9, trans(:, 6), 'b', 'DisplayName', '0.28 g/cm^3')
+plot (freq/1e9, trans(:, 7), 'r--', 'DisplayName', '0.41 g/cm^3')
+plot (freq/1e9, trans(:, 8), 'k--', 'DisplayName', '0.45 g/cm^3')
+plot (freq/1e9, trans(:, 9), 'k', 'DisplayName', '0.3 g/cm^3')
+plot (freq/1e9, trans(:, 10), 'g--', 'DisplayName', '0.33 g/cm^3')
+%ylim ([-10 -2])
+lgd = legend('location', 'southwest', 'orientation', 'vertical')
+title(lgd, 'LWC')
 set(gca, 'fontsize', fntsz)
 xlabel('Frequency (GHz)')
 ylabel ('Transmission (dB / Km)')
